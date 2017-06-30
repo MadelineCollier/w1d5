5f8537dff9e4b6994f12d2b04c2b60d0add5ad92
Madeline Collier <madel@telus.net>
2017-06-30 18:42:54 +0000

    added the exercises for the requiring modules workshop

---
+++ requiringModules/requiringModulesTwo.js
@@ -0,0 +1,12 @@
+var countLetters = require('./requiringModules.js');
+
+
+
+var givenString = "lighthouse in the house".toLowerCase();
+console.log(countLetters(givenString));
+
+
+
+
+var givenString = "HEY THERE".toLowerCase();
+console.log(countLetters(givenString));
